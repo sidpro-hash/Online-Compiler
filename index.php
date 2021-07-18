a7de9b30a9da5200aae230a79c76f79ccf57057d
Gabu Siddharth <gaburav567@gmail.com>
2021-07-18 16:31:36 +0530

    updated

--- index.php
+++ index.php
@@ -13,6 +13,27 @@
     <script src="https://cdnjs.cloudflare.com/ajax/libs/ace/1.4.12/ext-language_tools.min.js"></script>
     <script type="text/javascript" src="compiler.js"></script>
     <link rel="stylesheet" href="css/compiler.css">
+    <!-- icons -->
+    <link rel="apple-touch-icon" sizes="57x57" href="css/icons/favicon-57x57.png">
+    <link rel="apple-touch-icon" sizes="60x60" href="css/icons/favicon-60x60.png">
+    <link rel="apple-touch-icon" sizes="72x72" href="css/icons/favicon-72x72.png">
+    <link rel="apple-touch-icon" sizes="76x76" href="css/icons/favicon-76x76.png">
+    <link rel="apple-touch-icon" sizes="114x114" href="css/icons/favicon-114x114.png">
+    <link rel="apple-touch-icon" sizes="120x120" href="css/icons/favicon-120x120.png">
+    <link rel="apple-touch-icon" sizes="144x144" href="css/icons/favicon-144x144.png">
+    <link rel="apple-touch-icon" sizes="152x152" href="css/icons/favicon-152x152.png">
+    <link rel="apple-touch-icon" sizes="180x180" href="css/icons/favicon-180x180.png">
+    <link rel="icon" type="image/png" sizes="16x16" href="css/icons/favicon-16x16.png">
+    <link rel="icon" type="image/png" sizes="32x32" href="css/icons/favicon-32x32.png">
+    <link rel="icon" type="image/png" sizes="96x96" href="css/icons/favicon-96x96.png">
+    <link rel="icon" type="image/png" sizes="192x192" href="css/icons/favicon-192x192.png">
+    <link rel="shortcut icon" type="image/x-icon" href="css/icons/favicon.ico">
+    <link rel="icon" type="image/x-icon" href="css/icons/favicon.ico">
+    <meta name="msapplication-TileColor" content="#ffffff">
+    <meta name="msapplication-TileImage" content="css/icons/favicon-144x144.png">
+    <meta name="msapplication-config" content="css/icons/browserconfig.xml">
+    <link rel="manifest" href="css/icons/manifest.json">
+    <!-- icons -->
   </head>
   <body onload="ready()">
     <div class="container">
